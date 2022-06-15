5a41eb3af680ee4b4c59385ceb1b59a9137be7f1
AllerleiMal <emelpavel2002@gmail.com>
2022-06-15 19:25:49 +0300

    feat: implement input by typetext

--- Hotel/HotelUITests/SignInUITest.swift
+++ Hotel/HotelUITests/SignInUITest.swift
@@ -21,87 +21,21 @@ class SignInUITest: XCTestCase {
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
+
     func testSignIn() throws {
         let button = app.segmentedControls.buttons["Sign In"]
         button.tap()
         
-        app.windows.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .textField).element(boundBy: 0).tap()
-        
-        let tKey = app/*@START_MENU_TOKEN@*/.keys["t"]/*[[".keyboard!s.keys[\"t\"]",".keys[\"t\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        tKey.tap()
-        
-        let eKey = app/*@START_MENU_TOKEN@*/.keyboards.keys["e"]/*[[".keyboards.keys[\"e\"]",".keys[\"e\"]"],[[[-1,1],[-1,0]]],[1]]@END_MENU_TOKEN@*/
-        eKey.tap()
-        
-        let sKey = app/*@START_MENU_TOKEN@*/.keyboards.keys["s"]/*[[".keyboards.keys[\"s\"]",".keys[\"s\"]"],[[[-1,1],[-1,0]]],[1]]@END_MENU_TOKEN@*/
-        sKey.tap()
-        tKey.tap()
-        eKey.tap()
-        
-        let mKey = app/*@START_MENU_TOKEN@*/.keyboards.keys["m"]/*[[".keyboards.keys[\"m\"]",".keys[\"m\"]"],[[[-1,1],[-1,0]]],[1]]@END_MENU_TOKEN@*/
-        mKey.tap()
-        
-        let aKey = app/*@START_MENU_TOKEN@*/.keyboards.keys["a"]/*[[".keyboards.keys[\"a\"]",".keys[\"a\"]"],[[[-1,1],[-1,0]]],[1]]@END_MENU_TOKEN@*/
-        aKey.tap()
-        
-        let iKey = app/*@START_MENU_TOKEN@*/.keyboards.keys["i"]/*[[".keyboards.keys[\"i\"]",".keys[\"i\"]"],[[[-1,1],[-1,0]]],[1]]@END_MENU_TOKEN@*/
-        iKey.tap()
-        
-        let lKey = app/*@START_MENU_TOKEN@*/.keyboards.keys["l"]/*[[".keyboards.keys[\"l\"]",".keys[\"l\"]"],[[[-1,1],[-1,0]]],[1]]@END_MENU_TOKEN@*/
-        lKey.tap()
-        
-        let moreKey = app/*@START_MENU_TOKEN@*/.keyboards.keys["more"]/*[[".keyboards",".keys[\"numbers\"]",".keys[\"more\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[2,0]]@END_MENU_TOKEN@*/
-        moreKey.tap()
-        
-        let key = app/*@START_MENU_TOKEN@*/.keys["@"]/*[[".keyboards.keys[\"@\"]",".keys[\"@\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        key.tap()
-        
-        let moreKey2 = app/*@START_MENU_TOKEN@*/.keys["letters"]/*[[".keyboards",".keys[\"letters\"]",".keys[\"more\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[1]]@END_MENU_TOKEN@*/
-        moreKey2.tap()
-        
-        let gKey = app/*@START_MENU_TOKEN@*/.keys["g"]/*[[".keyboards.keys[\"g\"]",".keys[\"g\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        gKey.tap()
-        mKey.tap()
-        aKey.tap()
-        iKey.tap()
-        lKey.tap()
-        moreKey.tap()
-        
-        let key2 = app/*@START_MENU_TOKEN@*/.keys["."]/*[[".keyboards.keys[\".\"]",".keys[\".\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        key2.tap()
-        moreKey2.tap()
-        
-        let cKey = app/*@START_MENU_TOKEN@*/.keys["c"]/*[[".keyboards.keys[\"c\"]",".keys[\"c\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        cKey.tap()
-        
-        let oKey = app/*@START_MENU_TOKEN@*/.keys["o"]/*[[".keyboards.keys[\"o\"]",".keys[\"o\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        oKey.tap()
-        mKey.tap()
-        
-        let returnButton = app/*@START_MENU_TOKEN@*/.buttons["Return"]/*[[".keyboards",".buttons[\"return\"]",".buttons[\"Return\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/
-        returnButton.tap()
-        
-        let element = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element
-        element.children(matching: .textField).element(boundBy: 1).tap()
-        tKey.tap()
-        eKey.tap()
-        sKey.tap()
-        tKey.tap()
-        let pKey = app/*@START_MENU_TOKEN@*/.keys["p"]/*[[".keyboards.keys[\"p\"]",".keys[\"p\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        pKey.tap()
-        aKey.tap()
-        sKey.tap()
-        sKey.tap()
-        let wKey = app.keys["w"]
-        wKey.tap()
-        oKey.tap()
-        
-        let rKey = app/*@START_MENU_TOKEN@*/.keys["r"]/*[[".keyboards.keys[\"r\"]",".keys[\"r\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        rKey.tap()
-        let dKey = app.keys["d"]
-        dKey.tap()
+        let email = app.windows.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .textField).element(boundBy: 0)
+        email.tap()
+        email.typeText("testemail@gmail.com")
         
         let e = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element
+        e.tap()
+        
+        let element = app.windows.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .textField).element(boundBy: 1)
+        element.tap()
+        element.typeText("testpassword")
         e.tap()
         app.staticTexts["Sign"].tap()
         
